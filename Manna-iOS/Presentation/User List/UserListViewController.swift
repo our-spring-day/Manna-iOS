b97220d80cc6737d9046802277d15bb4879fecb5
jeongjaein <wodls96@gmail.com>
2020-06-10 21:23:12 +0900

    MVVM refactoring

--- Manna-iOS/Presentation/User List/UserListViewController.swift
+++ Manna-iOS/Presentation/User List/UserListViewController.swift
@@ -18,9 +18,8 @@ class UserListViewController: UIViewController {
     var disposeBag = DisposeBag()
     let tableView = UITableView()
     let searchController = UISearchController(searchResultsController: nil)
-    var userListViewModel = UserListViewModel()
+    var userListViewModel: UserListViewModel!
     var filteredFriends = [String]()
-    //    var friendsOb: Observable<[String]>
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -29,22 +28,12 @@ class UserListViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        friendsOb = Observable.of(filteredFriends)
         layout()
         tableViewAttribute()
         bind()
-        //searchResultupdater는 UISearchResultsUpdating 프로토콜을 따르는 UISearchController기반의 새로운 속성 입니다. 이 프로토콜은 UISearchBar 내의 텍스트가 변경되는것을 알립니다.
-        searchController.searchResultsUpdater = self
-        //기본적으로, UISearchController는 표시된 뷰를 흐리게(obscure) 만듭니다. 이것은 searchResultsController를 위해 다른 뷰 컨트롤러를 사용한다면 유용합니다. 여기에서는 결과를 표시하는것을 현재뷰로 설정했기 때문에 흐려지는걸 원하지 않습니다.
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "친구 검색"
-        //iOS11의 새로운 기능에서, NavigationItem으로 searchBar를 추가합니다. 이것은 Interface Builder가 UIsearchController와 아직 호환되지 않기 때문에 필요합니다.
-        navigationItem.searchController = searchController
-        //마지막으로 뷰 컨트롤러의 definesPresentationContext를 true로 설정하여 UISearchController가 활성화되어있는 동안 사용자가 다른 뷰 컨트롤러로 이동하면 search bar가 화면에 남아 있지 않도록 합니다.
-        definesPresentationContext = true
+        searchBarAttribute()
     }
     func tableViewAttribute() {
-        //        navigationController?.navigationBar.hidesBarsOnSwipe = true
         navigationItem.title = "친구"
         navigationController?.navigationBar.prefersLargeTitles = true
         tableView.do {
@@ -57,40 +46,53 @@ class UserListViewController: UIViewController {
         }
     }
     func layout() {
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+        tableView.do {
+            view.addSubview($0)
+            $0.snp.makeConstraints {
+                $0.edges.equalToSuperview()
+            }
+        }
+    }
+    //MARK: searchbar속성
+    //1. searchResultupdater는 UISearchResultsUpdating 프로토콜을 따르는 UISearchController기반의 새로운 속성 입니다. 이 프로토콜은 UISearchBar 내의 텍스트가 변경되는것을 알립니다.
+    //2. 기본적으로, UISearchController는 표시된 뷰를 흐리게(obscure) 만듭니다. 이것은 searchResultsController를 위해 다른 뷰 컨트롤러를 사용한다면 유용합니다. 여기에서는 결과를 표시하는것을 현재뷰로 설정했기 때문에 흐려지는걸 원하지 않습니다.
+    //4. iOS11의 새로운 기능에서, NavigationItem으로 searchBar를 추가합니다. 이것은 Interface Builder가 UIsearchController와 아직 호환되지 않기 때문에 필요합니다.
+    //5. 마지막으로 뷰 컨트롤러의 definesPresentationContext를 true로 설정하여 UISearchController가 활성화되어있는 동안 사용자가 다른 뷰 컨트롤러로 이동하면 search bar가 화면에 남아 있지 않도록 합니다.
+    func searchBarAttribute() {
+        searchController.do {
+            $0.searchResultsUpdater = self
+            $0.obscuresBackgroundDuringPresentation = false
+            $0.searchBar.placeholder = "친구 검색"
+            navigationItem.searchController = $0
+            definesPresentationContext = true
         }
     }
     func bind() {
-        //테이블뷰 셋
-        userListViewModel.outputs.friendsId
+        let input = userListViewModel.inputs(s
+        var userListViewModel = UserListViewModel(input: input)
+        userListViewModel.outputs.showFriends
             .bind(to: tableView.rx.items) {(tableView, row, item) -> UITableViewCell in
                 let cell = (tableView.dequeueReusableCell(withIdentifier: "UserListCell", for: IndexPath.init(row: row, section: 0)) as? UserListCell)!
                 cell.textLabel!.text = item
-                //                print(item)
                 return cell
         }
         .disposed(by: disposeBag)
         //검색기능]
-        searchController.searchBar.rx.text
-            .orEmpty
-            .distinctUntilChanged()
-            .debug()
-            .bind(to: userListViewModel.outputs.filteredFriendsId)
-            .disposed(by: disposeBag)
-        //        searchController.searchBar.rx.text
-        //            .orEmpty
-        //            .subscribe(onNext: {text in
-        //                self.filteredFriends = self.userListViewModel.outputs.testArr.filter { $0.contains(text) }
-        //                print(self.filteredFriends)
-        //                self.tableView.reloadData()
-        //            })
-        //            .disposed(by: disposeBag)
         
-        
-        
-        
+//        searchController.searchBar.rx.text
+//            .orEmpty
+//            .distinctUntilChanged()
+//            .debug()
+//            .bind(to: userListViewModel.inputs.searchKeyWord)
+//            .disposed(by: disposeBag)
+        //                searchController.searchBar.rx.text
+        //                    .orEmpty
+        //                    .subscribe(onNext: {text in
+        //                        self.filteredFriends = self.userListViewModel.outputs.testArr.filter { $0.contains(text) }
+        //                        print(self.filteredFriends)
+        //                        self.tableView.reloadData()
+        //                    })
+        //                    .disposed(by: disposeBag)
     }
 }
 extension UserListViewController: UISearchResultsUpdating {
--- Manna-iOS/Presentation/User List/UserListViewModel.swift
+++ Manna-iOS/Presentation/User List/UserListViewModel.swift
@@ -11,50 +11,51 @@ import RxSwift
 import RxCocoa
 import RxOptional
 
-protocol Input {
-    func searchFriends()
-    func detailFriends()
-    func deleteFriends()
-}
-protocol Output {
-    var friendsId: Observable<[String]> { get }
-    var filteredFriendsId: Variable<String> { get }
-}
+
 protocol Type {
+    associatedtype Input
+    associatedtype Output
+    
     var inputs: Input { get }
     var outputs: Output { get }
 }
-struct UserListViewModel: Type, Input, Output {
+class UserListViewModel: Type {
     let disposeBag = DisposeBag()
-    var filteredFriendsId: Variable<String> = Variable("")
-    let testArr: [String]
-    var friendsId: Observable<[String]>
-    lazy var filteredFridensIdObservable: Observable<String> = self.filteredFriendsId.asObservable()
-    func searchFriends() {
-        print("searchFriedns")
-    }
-    func detailFriends() {
-        print("detailFriendss")
-    }
-    func deleteFriends() {
-        print("deleteFriends")
-    }
+    let inputs: Input
+    let outputs: Output
     let userListModel: UserListModel
-    init() {
-        print("UserListViewModel Pass")
+    
+    init(input: Input) {
+        self.inputs = input
         userListModel = UserListModel()
-        testArr = userListModel.friends.sorted()
-        self.friendsId = Observable.of(testArr)
-        filteredFridensIdObservable.subscribe(onNext: {value in
-            print("Search value received = \(value)'")
-            
-            self.friendsId.map({ $0.filter({
-                if value.isEmpty {return true}
-                return ($0.lowercased().contains(value.lowercased()))
+        
+        let filteredFriends = input.searchKeyword.asObservable()
+            .subscribe(onNext: {skw in
+                self.userListModel.friends.filter{ $0.lowercased().contains(skw.lowercased())}
             })
-            })
-            }).disposed(by: disposeBag)
+        .disposed(by: disposeBag)
+        //        let filteredFriends = input.searchKeyword.asObservable()
+        //            .flatMap{ skw in self.userListModel.friends.filter {
+        //            $0.localizedCaseInsensitiveContains(skw)}}
+        //            });.disposed(by: disposeBag)
+        
+        //        filteredFridensIdObservable.subscribe(onNext: {value in
+        //            print("Search value received = \(value)'")
+        //
+        //            self.friendsId.map({ $0.filter({
+        //                if value.isEmpty {return true}
+        //                return ($0.lowercased().contains(value.lowercased()))
+        //            })
+        //            })
+        //            }).disposed(by: disposeBag)
+        self.outputs = Output(showFriends: filteredFriends)
     }
-    var inputs: Input { return self }
-    var outputs: Output { return self }
+}
+extension UserListViewModel {
+    struct Input {
+        let searchKeyword: Observable<String>
+    }
+    struct Output {
+        var showFriends: Observable<String>
+    }
 }
