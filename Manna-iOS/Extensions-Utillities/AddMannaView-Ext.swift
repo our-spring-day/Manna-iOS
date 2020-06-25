1c4941221cb8176be3c2d5aa33f0e9a573fe1a40
s-doris <swdoriz@gmail.com>
2020-06-25 23:42:59 +0900

    Add addMannaView-Ext : add func alert

---
+++ Manna-iOS/Extensions-Utillities/AddMannaView-Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIView-Ext.swift
+//  Manna-iOS
+//
+//  Created by once on 2020/06/25.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+extension AddMannaViewController {
+    func alert(title: String = "알림", message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
---
+++ Manna-iOS/Presentation/Manna List/AddManna/PeopleAddMannaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PeopleAddMannaViewController.swift
+//  Manna-iOS
+//
+//  Created by once on 2020/06/25.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import UIKit
+
+class PeopleAddMannaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
