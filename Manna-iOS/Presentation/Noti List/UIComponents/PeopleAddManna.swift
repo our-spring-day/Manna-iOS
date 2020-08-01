6e4fbe1abca1fc1fa5d11d3fffd0129f4b749150
s-doris <swdoriz@gmail.com>
2020-08-02 03:28:44 +0900

    Add people view component

--- Manna-iOS/Presentation/Noti List/UIComponents/PeopleAddManna.swift
+++ Manna-iOS/Presentation/Noti List/UIComponents/PeopleAddManna.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2020 정재인. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Then
+
+class PeopleAddManna: UIView {
+    
+    lazy var mannaPeople = UITextField()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        layout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder) 
+    }
+    
+    func attribute() {
+        backgroundColor = .gray
+        mannaPeople.do {
+            $0.layer.borderWidth = 1.0
+            $0.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
+    
+    func layout() {
+        addSubview(mannaPeople)
+        mannaPeople.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(20)
+            $0.width.equalTo(200)
+            $0.height.equalTo(40)
+        }
+    }
+}
