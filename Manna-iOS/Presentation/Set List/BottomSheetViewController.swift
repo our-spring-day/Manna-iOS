505435572b394b3563d8000a7dd7071f587c4146
jeongjaein <wodls96@gmail.com>
2020-08-05 19:42:52 +0900

    [Add] dynamic bottomsheet with pan gesture

--- Manna-iOS/Presentation/Set List/BottomSheetViewController.swift
+++ Manna-iOS/Presentation/Set List/BottomSheetViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 extension BottomSheetViewController {
     private enum State {
@@ -21,12 +23,14 @@ extension BottomSheetViewController {
 }
 
 class BottomSheetViewController: UIViewController {
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let gesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture))
         view.addGestureRecognizer(gesture)
         roundViews()
+        
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -36,13 +40,14 @@ class BottomSheetViewController: UIViewController {
     
     private func moveView(state: State) {
         let yPosition = state == .partial ? Constant.partialViewYPosition : Constant.fullViewYPosition
+        
         view.frame = CGRect(x: 0, y: yPosition, width: view.frame.width, height: view.frame.height)
     }
     
     private func moveView(panGestureRecognizer recognizer: UIPanGestureRecognizer) {
         let translation = recognizer.translation(in: view)
-        let minY = view.frame.minY
         
+        let minY = view.frame.minY
         if (minY + translation.y >= Constant.fullViewYPosition) && (minY + translation.y <= Constant.partialViewYPosition) {
             view.frame = CGRect(x: 0, y: minY, width: view.frame.width, height: view.frame.height)
             recognizer.setTranslation(CGPoint.zero, in: view)
@@ -56,6 +61,10 @@ class BottomSheetViewController: UIViewController {
             UIView.animate(withDuration: 1, delay: 0.0, options: [.allowUserInteraction], animations: { let state: State = recognizer.velocity(in: self.view).y >= 0 ? .partial : .full
                 self.moveView(state: state)
             }, completion: nil)
+        } else if recognizer.state == .changed {
+            let changedY = view.center.y + recognizer.location(in: view).y
+
+            view.center = CGPoint(x: view.center.x, y: changedY)
         }
     }
     
