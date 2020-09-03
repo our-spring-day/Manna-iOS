e0c67a6f26daa615b6054a4b4003b88c950bef50
s-doris <swdoriz@gmail.com>
2020-09-03 23:43:49 +0900

    feat : SelectPlaceModel class 초기화

---
+++ Manna-iOS/Presentation/Manna List/AddManna/SelectPlaceModel.swift
@@ -0,0 +1,22 @@
+//
+//  SelectPlaceModel.swift
+//  Manna-iOS
+//
+//  Created by once on 2020/09/03.
+//  Copyright © 2020 정재인. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class SelectPlaceModel {
+    let addressNetwork: AddressFetchable
+    
+    init(addressNetwork: AddressFetchable = AddressAPI()) {
+        self.addressNetwork = addressNetwork
+    }
+    
+    func getAdress(address: String) -> Observable<Result<[Address], Error>> {
+        return addressNetwork.getAddress2(address)
+    }
+}
