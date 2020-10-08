0ed9ecea7c962b74b7250f236873b562abc0ad54
jeongjaein <wodls96@gmail.com>
2020-10-08 17:04:41 +0900

    feat : 마커 구석에 찍기 zoomin zoomout 과 상관없음

--- Manna-iOS/Presentation/SetList/SetListViewController.swift
+++ Manna-iOS/Presentation/SetList/SetListViewController.swift
@@ -26,7 +26,7 @@ class SetListViewController: UIViewController {
     let marker = NMFMarker()
     var viewModel = DurringMeetingViewModel()
     var test: NMGLatLngBounds?
-    var cameraflag = true
+    var cameraflag = 0.00001
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,13 +44,16 @@ class SetListViewController: UIViewController {
             $0.positionMode = .direction
         }
         marker.do {
+//            $0.iconImage = NMFOverlayImage(image: #imageLiteral(resourceName: "logo"))
+            $0.width = 30
+            $0.height = 40
             $0.position = NMGLatLng(lat: 37.411677, lng: 127.128621)
             $0.mapView = nmapFView
         }
         bottomSheet = BottomSheetViewController(frame: CGRect(x: 0,
                                                               y: 0,
                                                               width: UIScreen.main.bounds.width,
-                                                              height: UIScreen.main.bounds.height/2),viewModel: viewModel)
+                                                              height: UIScreen.main.bounds.height/2), viewModel: viewModel)
         bottomSheet?.do {
             $0.backgroundColor = .white
             $0.alpha = 0.9
@@ -83,7 +86,7 @@ class SetListViewController: UIViewController {
     func bind() {
         //나중에 bind를 통해서 컬렉션뷰에 뿌려줄 item 현재는 되는지 만 확인 했고 됨
         viewModel.meetingInfo.subscribe(onNext: {
-            print("이거 탑니까>???????",$0)
+            print("이거 탑니까>???????", $0)
             
         }).disposed(by: disposeBag)
         
@@ -92,7 +95,7 @@ class SetListViewController: UIViewController {
                 let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: $0.currentLocation.lat, lng: $0.currentLocation.lng))
                 cameraUpdate.animation = .easeOut
                 self.nmapFView.moveCamera(cameraUpdate)
-                cameraflag.toggle()
+                //                cameraflag.toggle()
             }).disposed(by: disposeBag)
     }
     @objc func compareLatLng() {
@@ -104,26 +107,65 @@ extension SetListViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-//        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        //        print("locations = \(locValue.latitude) \(locValue.longitude)")
         locationOverlay.location = NMGLatLng(lat: locValue.latitude, lng: locValue.longitude)
     }
 }
 
 extension SetListViewController: NMFMapViewCameraDelegate {
     func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
-        
-        marker.position = NMGLatLng(lat: nmapFView.cameraPosition.target.lat, lng: nmapFView.cameraPosition.target.lng)
+//        print(nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lat)
+//        print(nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).northEast.lat)
+//        print(nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame - 10).northEast.lat)
+//        print(nmapFView.projection.latlng(from: CGPoint.init(x: -200, y: -200)).lat)
+        print(marker.width)
+//        marker.position = NMGLatLng(
+//            lat: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lat - nmapFView.projection.latlng(from: CGPoint.init(x: 100, y: 100)).lat,
+//            lng: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lng - nmapFView.projection.latlng(from: CGPoint.init(x: 100, y: 100)).lng
+//        )
+//        marker.position = NMGLatLng(
+//            lat: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lat + 0.001,
+//            lng: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lng + 0.001
+//        )
+        marker.position = NMGLatLng(
+            lat: nmapFView.projection.latlng(from: CGPoint.init(x: view.frame.maxX - (marker.width / 2), y: 200)).lat,
+            lng: nmapFView.projection.latlng(from: CGPoint.init(x: view.frame.maxX - (marker.width / 2), y: 200)).lng
+        )
         marker.mapView = nmapFView
-
     }
+    
     func mapView(_ mapView: NMFMapView, cameraDidChangeByReason reason: Int, animated: Bool) {
-        marker.position = NMGLatLng(lat: nmapFView.cameraPosition.target.lat, lng: nmapFView.cameraPosition.target.lng)
-        marker.mapView = nmapFView
+//        marker.position = NMGLatLng(
+//            lat: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lat - nmapFView.projection.latlng(from: CGPoint.init(x: 100, y: 100)).lat,
+//            lng: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lng - nmapFView.projection.latlng(from: CGPoint.init(x: 100, y: 100)).lng
+//        )
+//        marker.position = NMGLatLng(
+//            lat: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lat + 0.001,
+//            lng: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lng + 0.001
+//        )
+        
+        marker.position = NMGLatLng(
+            lat: nmapFView.projection.latlng(from: CGPoint.init(x: view.frame.maxX - (marker.width / 2), y: 200)).lat,
+            lng: nmapFView.projection.latlng(from: CGPoint.init(x: view.frame.maxX - (marker.width / 2), y: 200)).lng
+        )
 
+        marker.mapView = nmapFView
     }
+    
     func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
-        marker.position = NMGLatLng(lat: nmapFView.cameraPosition.target.lat, lng: nmapFView.cameraPosition.target.lng)
-        marker.mapView = nmapFView
+//        marker.position = NMGLatLng(
+//            lat: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lat - nmapFView.projection.latlng(from: CGPoint.init(x: 100, y: 100)).lat,
+//            lng: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lng - nmapFView.projection.latlng(from: CGPoint.init(x: 100, y: 100)).lng
+//        )
+//        marker.position = NMGLatLng(
+//            lat: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lat + 0.001,
+//            lng: nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest.lng + 0.001
+//        )
+        marker.position = NMGLatLng(
+            lat: nmapFView.projection.latlng(from: CGPoint.init(x: view.frame.maxX - (marker.width / 2), y: 200)).lat,
+            lng: nmapFView.projection.latlng(from: CGPoint.init(x: view.frame.maxX - (marker.width / 2), y: 200)).lng
+        )
 
+        marker.mapView = nmapFView
     }
 }
