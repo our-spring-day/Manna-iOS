6a4041a1f9848572d5760ed49b7e3bda7c1b4151
jeongjaein <wodls96@gmail.com>
2020-10-16 18:46:12 +0900

    feat : line추가, 카메라 회전 대응

--- Manna-iOS/Presentation/SetList/MapView.swift
+++ Manna-iOS/Presentation/SetList/MapView.swift
@@ -24,6 +24,7 @@ class MapView: UIViewController {
     let inviteFriensViewModel = InviteFriendsViewModel()
     var locationOverlay = NMFMapView().locationOverlay
     let marker = NMFMarker()
+    let testmarker = NMFMarker()
     var viewModel = DurringMeetingViewModel()
     
     var testTargetLat = 37.478566
@@ -32,12 +33,13 @@ class MapView: UIViewController {
     
     var centerLat: Double = 0
     var centerLng: Double = 0
-    
-    var southWest = NMGLatLng()
-    var northEast = NMGLatLng()
-    
     var latValue: Double = 0
     var lngValue: Double = 0
+    
+    var aLine = NMFPath()
+    var bLine = NMFPath()
+    var cLine = NMFPath()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -117,6 +119,36 @@ class MapView: UIViewController {
         return (y2-y1) / (x2-x1) * key + (y1 - ((y2-y1)/(x2-x1) * x1))
     }
     
+    func getLinearEquation(x1: Double, x2: Double, y1: Double, y2: Double)  -> [Double] {
+        let inclination = ((y2-y1) / (x2-x1))
+        let constant = (y1 - ((y2-y1)/(x2-x1) * x1))
+        return [inclination, constant]
+    }
+    
+    func getCrossPoint(centerLng: Double,
+                       centerLat: Double,
+                       targetLng: Double,
+                       targetLat: Double,
+                       frameLng1: Double,
+                       frameLat1: Double,
+                       frameLng2: Double,
+                       frameLat2: Double) -> [Double] {
+        //a1,b1
+        var first = getLinearEquation(x1: centerLng, x2: targetLng, y1: centerLat, y2: targetLat)
+        //a2,b2
+        var second = getLinearEquation(x1: frameLng1, x2: frameLng2, y1: frameLat1, y2: frameLat2)
+        
+        if second[0].isInfinite {
+            //x = b 일때 분기
+            return [frameLng1, (first[0] * frameLng1) + first[1]]
+        }
+//             (- (b1       -        b2) / (a1       -       a2), a1       * ( -(b1       -        b2) / (a1       -        a2)) +       b1)
+        var newLng = -((first[1] - second[1]) / (first[0] - second[0]))
+        var newLat = first[0] * ( -(first[1] - second[1]) / (first[0] - second[0]))
+        
+        return [newLng, newLat + first[1]]
+    }
+    
     func result(yPosition: Double? = nil, xPosition: Double? = nil) -> Double {
         if let key = yPosition {
             return getX(x1: centerLng, x2: testTargetLng, y1: centerLat, y2: testTargetLat, key: key)
@@ -129,31 +161,101 @@ class MapView: UIViewController {
         centerLat = nmapFView.cameraPosition.target.lat
         centerLng = nmapFView.cameraPosition.target.lng
         
-        southWest = nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).southWest
-        northEast = nmapFView.projection.latlngBounds(fromViewBounds: self.view.frame).northEast
+        var southWest = nmapFView.projection.latlng(from: CGPoint(x: 0, y: view.frame.height))
+        var northEast = nmapFView.projection.latlng(from: CGPoint(x: view.frame.width, y: 0))
+        var northWest = nmapFView.projection.latlng(from: CGPoint(x: 0, y: 0))
+        var southEast = nmapFView.projection.latlng(from: CGPoint(x: view.frame.width, y: view.frame.height))
         
-        if southWest.lng < testTargetLng && testTargetLng < northEast.lng && southWest.lat < testTargetLat && testTargetLat < northEast.lat {
+        aLine.path = NMGLineString(points: [
+            NMGLatLng(lat: southWest.lat, lng: southWest.lng),
+            NMGLatLng(lat: northEast.lat, lng: northEast.lng)
+        ])
+        aLine.color = .blue
+        aLine.mapView = nmapFView
+        
+        bLine.path = NMGLineString(points: [
+            NMGLatLng(lat: northWest.lat, lng: northWest.lng),
+            NMGLatLng(lat: southEast.lat, lng: southEast.lng)
+        ])
+        bLine.color = .blue
+        bLine.mapView = nmapFView
+
+        var test = NMGLatLngBounds(southWest: southWest, northEast: northEast)
+        nmapFView.projection.latlngBounds(fromViewBounds: view.frame)
+        
+        cLine.path = NMGLineString(points: [
+            NMGLatLng(lat: nmapFView.projection.latlngBounds(fromViewBounds: view.frame).southWestLat, lng: nmapFView.projection.latlngBounds(fromViewBounds: view.frame).southWestLng),
+            NMGLatLng(lat: nmapFView.projection.latlngBounds(fromViewBounds: view.frame).northEastLat, lng: nmapFView.projection.latlngBounds(fromViewBounds: view.frame).northEastLng)
+        ])
+        cLine.color = .red
+        cLine.mapView = nmapFView
+        
+        
+        if nmapFView.projection.point(from: NMGLatLng(lat: testTargetLat, lng: testTargetLng)).x < view.frame.width &&
+            0 < nmapFView.projection.point(from: NMGLatLng(lat: testTargetLat, lng: testTargetLng)).x &&
+            nmapFView.projection.point(from: NMGLatLng(lat: testTargetLat, lng: testTargetLng)).y < view.frame.height &&
+            0 < nmapFView.projection.point(from: NMGLatLng(lat: testTargetLat, lng: testTargetLng)).y
+            {
             marker.position = NMGLatLng(lat: testTargetLat, lng: testTargetLng)
             marker.mapView = nmapFView
         } else {
-            if testTargetLat < getY(x1: southWest.lng, x2: northEast.lng, y1: southWest.lat, y2: northEast.lat, key: testTargetLng)  {
-                if testTargetLat < getY(x1: southWest.lng, x2: northEast.lng, y1: northEast.lat, y2: southWest.lat, key: testTargetLng) {
+            if testTargetLat < getY(x1: southWest.lng, x2: northEast.lng, y1: southWest.lat, y2: northEast.lat, key: testTargetLng) {
+                print("A보다 밑이야")
+                if testTargetLat < getY(x1: northWest.lng, x2: southEast.lng, y1: northWest.lat, y2: southEast.lat, key: testTargetLng) {
                     //밑
-                    marker.position = NMGLatLng(lat: southWest.lat, lng: result(yPosition: southWest.lat))
+                    print("밑")
+                    var newLatLng = getCrossPoint(centerLng: centerLng,
+                                                 centerLat: centerLat,
+                                                 targetLng: testTargetLng,
+                                                 targetLat: testTargetLat,
+                                                 frameLng1: southWest.lng,
+                                                 frameLat1: southWest.lat,
+                                                 frameLng2: southEast.lng,
+                                                 frameLat2: southEast.lat)
+                    marker.position = NMGLatLng(lat: newLatLng[1], lng: newLatLng[0])
                     marker.mapView = nmapFView
                 } else {
                     //오른
-                    marker.position = NMGLatLng(lat: result(xPosition: northEast.lng), lng: northEast.lng)
+                    print("오른")
+                    var newLatLng = getCrossPoint(centerLng: centerLng,
+                                                  centerLat: centerLat,
+                                                  targetLng: testTargetLng,
+                                                  targetLat: testTargetLat,
+                                                  frameLng1: northEast.lng,
+                                                  frameLat1: northEast.lat,
+                                                  frameLng2: southEast.lng,
+                                                  frameLat2: southEast.lat)
+                    marker.position = NMGLatLng(lat: newLatLng[1], lng: newLatLng[0])
                     marker.mapView = nmapFView
                 }
             } else {
-                if testTargetLat < getY(x1: southWest.lng, x2: northEast.lng, y1: northEast.lat, y2: southWest.lat, key: testTargetLng) {
+                //A 보다 위야
+                print("A보다 위야")
+                if testTargetLat < getY(x1: northWest.lng, x2: southEast.lng, y1: northWest.lat, y2: southEast.lat, key: testTargetLng) {
                     //왼
-                    marker.position = NMGLatLng(lat: result(xPosition: southWest.lng), lng: southWest.lng)
+                    print("왼")
+                    var newLatLng = getCrossPoint(centerLng: centerLng,
+                                                  centerLat: centerLat,
+                                                  targetLng: testTargetLng,
+                                                  targetLat: testTargetLat,
+                                                  frameLng1: northWest.lng,
+                                                  frameLat1: northWest.lat,
+                                                  frameLng2: southWest.lng,
+                                                  frameLat2: southWest.lat)
+                    marker.position = NMGLatLng(lat: newLatLng[1], lng: newLatLng[0])
                     marker.mapView = nmapFView
                 } else {
                     //위
-                    marker.position = NMGLatLng(lat: nmapFView.projection.latlng(from: CGPoint(x: 0, y: view.bounds.minY + marker.height)).lat, lng: result(yPosition: northEast.lat))
+                    print("위")
+                    var newLatLng = getCrossPoint(centerLng: centerLng,
+                                                  centerLat: centerLat,
+                                                  targetLng: testTargetLng,
+                                                  targetLat: testTargetLat,
+                                                  frameLng1: northWest.lng,
+                                                  frameLat1: northWest.lat,
+                                                  frameLng2: northEast.lng,
+                                                  frameLat2: northEast.lat)
+                    marker.position = NMGLatLng(lat: newLatLng[1], lng: newLatLng[0])
                     marker.mapView = nmapFView
                 }
             }
